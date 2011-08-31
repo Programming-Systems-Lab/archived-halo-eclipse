ae8d214de6f925eb9e1c4330140d9124180d83c9
Jonathan Bell <jbell@cs.columbia.edu>
2011-08-31 17:06:24 -0400

    HALO-3

--- halo-EJB/ejbModule/edu/columbia/cs/psl/halo/server/AbstractFacade.java
+++ halo-EJB/ejbModule/edu/columbia/cs/psl/halo/server/AbstractFacade.java
@@ -5,15 +5,22 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 import javax.ejb.SessionContext;
+import javax.jws.WebMethod;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
 
+import edu.columbia.cs.psl.halo.entity.LogAction;
+import edu.columbia.cs.psl.halo.entity.LogEntry;
 import edu.columbia.cs.psl.halo.entity.User;
 
 
@@ -148,4 +155,59 @@ public abstract class AbstractFacade<T> {
         return ((Long) q.getSingleResult()).intValue();
     }
 
+	protected void log(String action, String params)
+	{
+		LogEntry e = new LogEntry();
+		e.setAction(getAction(action));
+		e.setParams(params);
+		e.setCreatedBy(getUser());
+		e.setIp(getRemoteIP());
+		e.setCreatedAt(new Date());
+		getEntityManager().persist(e);
+	}
+	
+	private LogAction getAction(String action)
+	{
+		Query cq = getEntityManager().createNativeQuery("SELECT * FROM LogAction where log_key=? LIMIT 1", LogAction.class);
+		cq.setParameter(1, action);
+		LogAction ret = null;
+		try
+		{
+			ret = (LogAction) cq.getSingleResult();
+		}
+		catch(Exception ex)
+		{
+			ret = new LogAction();
+			ret.setKey(action);
+			getEntityManager().persist(ret);
+		}
+		return ret;
+	}
+	
+	@Resource
+	WebServiceContext wsContex;
+	protected long getRemoteIP()
+	{
+		if(wsContex == null)
+			return 0;
+
+		try
+		{
+		MessageContext mc = wsContex.getMessageContext();
+		HttpServletRequest req = (HttpServletRequest)mc.get(MessageContext.SERVLET_REQUEST); 
+		String[] addrArray = req.getRemoteAddr().split("\\.");
+
+		long num = 0;
+		for (int i=0;i<addrArray.length;i++) {
+			int power = 3-i;
+
+			num += ((Integer.parseInt(addrArray[i])%256 * Math.pow(256,power)));
+		}
+		return num;
+		}
+		catch(Exception ex)
+		{
+			return 0;
+		}
+	}
 }
--- halo-EJB/ejbModule/edu/columbia/cs/psl/halo/server/LogService.java
+++ halo-EJB/ejbModule/edu/columbia/cs/psl/halo/server/LogService.java
@@ -28,57 +28,6 @@ public class LogService extends AbstractFacade<LogEntry> {
 	@WebMethod
 	public void log(String action, String params)
 	{
-		LogEntry e = new LogEntry();
-		e.setAction(getAction(action));
-		e.setParams(params);
-		e.setCreatedBy(getUser());
-		e.setIp(getRemoteIP());
-		e.setCreatedAt(new Date());
-		getEntityManager().persist(e);
-	}
-	
-	private LogAction getAction(String action)
-	{
-		Query cq = getEntityManager().createNativeQuery("SELECT * FROM LogAction where log_key=? LIMIT 1", LogAction.class);
-		cq.setParameter(1, action);
-		LogAction ret = null;
-		try
-		{
-			ret = (LogAction) cq.getSingleResult();
-		}
-		catch(Exception ex)
-		{
-			ret = new LogAction();
-			ret.setKey(action);
-			getEntityManager().persist(ret);
-		}
-		return ret;
-	}
-	
-	@Resource
-	WebServiceContext wsContex;
-	protected long getRemoteIP()
-	{
-		if(wsContex == null)
-			return 0;
-
-		try
-		{
-		MessageContext mc = wsContex.getMessageContext();
-		HttpServletRequest req = (HttpServletRequest)mc.get(MessageContext.SERVLET_REQUEST); 
-		String[] addrArray = req.getRemoteAddr().split("\\.");
-
-		long num = 0;
-		for (int i=0;i<addrArray.length;i++) {
-			int power = 3-i;
-
-			num += ((Integer.parseInt(addrArray[i])%256 * Math.pow(256,power)));
-		}
-		return num;
-		}
-		catch(Exception ex)
-		{
-			return 0;
-		}
+		super.log(action, params);
 	}
 }
